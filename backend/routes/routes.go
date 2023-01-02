2fd8fd3d86aa247ffe6216bb5fcc1f5334d62695
Sahil Mandaliya <sahilmandaliya@Sahils-MacBook-Air.local>
2023-01-02 16:17:38 +0530

    Empty-Commit

---
+++ backend/routes/routes.go
@@ -0,0 +1,40 @@
+package routes
+
+import (
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+func Routers() {
+
+	router := mux.NewRouter()
+	http.ListenAndServe(":9080",
+		&CORSRouterDecorator{router})
+}
+
+/***************************************************/
+
+// CORSRouterDecorator applies CORS headers to a mux.Router
+type CORSRouterDecorator struct {
+	R *mux.Router
+}
+
+func (c *CORSRouterDecorator) ServeHTTP(rw http.ResponseWriter,
+	req *http.Request) {
+	if origin := req.Header.Get("Origin"); origin != "" {
+		rw.Header().Set("Access-Control-Allow-Origin", origin)
+		rw.Header().Set("Access-Control-Allow-Methods",
+			"POST, GET, OPTIONS, PUT, DELETE")
+		rw.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Accept-Language,"+
+				" Content-Type, YourOwnHeader")
+	}
+	// Stop here if its Preflighted OPTIONS request
+	if req.Method == "OPTIONS" {
+		return
+	}
+
+	c.R.ServeHTTP(rw, req)
+}
